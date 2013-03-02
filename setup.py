8716a6cc0be979e50a8cbfeac0caefefa3b21a14
Laurens Van Houtven <_@lvh.cc>
2013-03-02 13:01:24 +0100

    Add version information

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
+import re
+versionLine = open("txscrypt/_version.py", "rt").read()
+match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", versionLine, re.M)
+versionString = match.group(1)
+
 setup(name='txscrypt',
-      version='20121015',
+      version=versionString,
       description='Twisted wrapper for scrypt',
       url='https://github.com/lvh/txscrypt',
 
@@ -19,4 +24,3 @@ setup(name='txscrypt',
         "Framework :: Twisted",
         "License :: OSI Approved :: ISC License (ISCL)",
         ])
-
--- txscrypt/__init__.py
+++ txscrypt/__init__.py
@@ -2,5 +2,6 @@
 A Twisted-friendly wrapper for scrypt.
 """
 from txscrypt.wrapper import computeKey, verifyPassword
+from txscrypt._version import __version__
 
 __all__ = ["computeKey", "verifyPassword"]
