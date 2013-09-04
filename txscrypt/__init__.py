38ce0d6b0433a68787c18691407c815d4eb1fdb2
Laurens Van Houtven <_@lvh.cc>
2013-09-04 17:28:16 +0200

    Make checkPassword the only public API, remove verifyPassword

--- txscrypt/__init__.py
+++ txscrypt/__init__.py
@@ -1,7 +1,7 @@
 """
 A Twisted-friendly wrapper for scrypt.
 """
-from txscrypt.wrapper import computeKey, verifyPassword
+from txscrypt.wrapper import checkPassword, computeKey
 from txscrypt._version import __version__
 
-__all__ = ["computeKey", "verifyPassword"]
+__all__ = ["verifyPassword", "computeKey"]
